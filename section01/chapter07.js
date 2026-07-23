5067b5da974aeb2b1356d9b36c48e7a1dbcb6cf8
anuma8080-bit <anuma8080@gmail.com>
2026-07-23 20:39:45 +0900

    자바스크립트 연산자 1 학습

---
+++ section01/chapter07.js
@@ -0,0 +1,31 @@
+let var1 = 1;
+
+let num =3+2;
+let num1 = 3-2;
+let num2 = 3*2;
+let num3 = 3/2;
+let num4 = 3%2;
+
+let num5 = (1+2*10);
+
+
+let num6 = 10;
+num6+= 20;
+num6-=20;
+num6*=20;
+num6/=20;
+num6%=10;
+
+
+
+let or = true||false;
+let and = true&&false;
+let not = !true;
+
+let com1 = 1===2;
+let com2 = 1!==2;
+
+let com3 = 2>3;
+let com4 = 2<3;
+let com5 = 2<=3;
+let com6 = 2>=3;
