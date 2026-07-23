f6318ed02b3e58b127345262b1d2ad3834992ca3
anuma8080-bit <anuma8080@gmail.com>
2026-07-23 19:42:05 +0900

    Initial commit

---
+++ chapter04.js
@@ -0,0 +1,22 @@
+let age= 27;
+//console.log(age);
+age = 30;
+const birthday=123;
+
+let $_name;
+
+let name1;
+//let 1name
+
+
+//let if
+
+//가이드
+
+let a = 1;//내용알기어려움
+let b = 2;
+let c = a-b;
+
+let salecount = 1;
+let refundC0unt = 1;
+let totoalSalecount = salecount- refundC0unt;
