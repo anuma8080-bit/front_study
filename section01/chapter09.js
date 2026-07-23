56728e43bcc1a46dc50031302604d8febcbff8bc
anuma8080-bit <anuma8080@gmail.com>
2026-07-23 21:15:00 +0900

    조건문 학습

---
+++ section01/chapter09.js
@@ -0,0 +1,35 @@
+let num = 6;
+if(num>=10){
+    console.log("조건인 참입니다");
+    console.log("10이상이에요");
+}else if(num>=5){
+    console.log("5이상이에요");
+}else if(num>=4){
+    console.lof('4이상이에용');
+}else{
+    console.log("조건에 안 맞아용");
+}
+
+
+
+let animal ="human";
+
+switch(animal){
+    case "cat" :{
+        console.log('고양이');
+        break;
+    }
+    case "bear":{
+        console.log('고양이');
+        break;
+    }
+    case "fish":{
+        console.log('고양이');
+        break;}
+    case "tiger":{
+        console.log('고양이');
+        break;
+    }default:{
+        console.log('난 그런거 몰라용');
+    }
+}
