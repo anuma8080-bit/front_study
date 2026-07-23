38e535fe0508f3b55a460da2dc14ac798bafbab4
anuma8080-bit <anuma8080@gmail.com>
2026-07-23 21:26:37 +0900

    반복문 학습

---
+++ section01/chapter10.js
@@ -0,0 +1,18 @@
+for(let idx = 1;idx <= 10; idx++){
+    console.log(idx);
+}
+
+for(let idx = 1;idx <= 10; idx++){
+    if(idx>=5){
+        break;
+    }
+    console.log(idx);
+}
+
+
+for(let idx = 1;idx <= 10; idx++){
+    if(idx%2===0){
+        continue;
+    }
+    console.log(idx);
+}
