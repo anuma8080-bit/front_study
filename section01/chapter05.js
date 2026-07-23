2acf72aa6f5761323aade922f40c28ec57fc31f6
anuma8080-bit <anuma8080@gmail.com>
2026-07-23 20:04:12 +0900

    자바스크립의 자료형 공부

---
+++ section01/chapter05.js
@@ -0,0 +1,33 @@
+//1. numbertype 
+
+let num1=26;
+let num2=1.5;
+let num3=-20;
+
+console.log(num1+num2);
+console.log(num1-num2);
+console.log(num1*num2);
+console.log(num1/num2);
+console.log(num1%num2);
+
+let inf = Infinity;
+let minf = -Infinity;
+let nan = NaN;
+
+console.log(1*"hello");
+let myname = "전지훈";
+let myLocation="진해";
+let introduce = myname+myLocation;
+console.log(introduce);
+
+let myintroduce = `${myname}은 ${myLocation}에 살아요`;
+console.log(myintroduce)
+
+let isSwitchOn = true;
+let isEmpty = false;
+
+
+let empty = null;
+
+let none;
+console.log(none);
