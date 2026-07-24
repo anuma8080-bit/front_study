2571e265c0b4308f072f77442cea7dda1f1602fe
anuma8080-bit <anuma8080@gmail.com>
2026-07-24 16:41:23 +0900

    함수표현식과 화살표 함수학습

---
+++ section01/chapter12.js
@@ -0,0 +1,27 @@
+function funcA(){
+    console.log("funA");
+}
+
+let varA = funcA;
+
+varA();
+
+let varB = function(){// 호스팅안됨
+    console.log("funB");
+}
+
+varB();
+
+let varC = () => {//호스팅 안됨
+    console.log("varC");
+}
+
+varC()
+
+let varD = (value) =>{
+    console.log(value);
+    return value+10;
+}
+
+console.log(varD);
+console.log(varD(10));
