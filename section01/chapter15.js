ab7527b7b8f32beefa2f57195f89db76b8883555
anuma8080-bit <anuma8080@gmail.com>
2026-07-26 20:46:18 +0900

    객체 학습

---
+++ section01/chapter15.js
@@ -0,0 +1,23 @@
+let obj2 = {};
+
+
+let person = {
+    name : '전지훈',
+    age : 17,
+    job : 'student',
+
+}
+
+let name = person.name;
+console.log(name);
+let age = person['age'];
+console.log(age);
+person.favoriteanimal = '강아지';
+person['hobby'] = '게임';
+
+console.log(person);
+
+
+delete person.job;
+
+let result = 'name' in person;
